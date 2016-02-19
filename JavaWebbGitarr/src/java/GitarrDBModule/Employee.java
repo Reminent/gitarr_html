792016cca11f9a096169d17448086f1889fee070
Martin <Martin@Martin-dator>
2016-02-19 12:34:58 +0100

    vältartest

--- JavaWebbGitarr/src/java/GitarrDBModule/Employee.java
+++ JavaWebbGitarr/src/java/GitarrDBModule/Employee.java
@@ -2,9 +2,10 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+Vältarn test!!!! GGRIP
  */
 package GitarrDBModule;
-
+//Test
 import java.io.Serializable;
 import java.util.Collection;
 import javax.persistence.Basic;
